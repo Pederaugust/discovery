7abaf8cc2bea566bea9247459c3f91873167bed0
pederaugust <peder.august@protonmail.com>
2021-03-23 08:49:23 +0100

    Did all the tasks up to 08

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,22 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let half_period = 50_u16;
+    let led_amount = 8;
+
+    loop {
+        for x in 0..led_amount {
+            leds[x].on().ok();
+            delay.delay_ms(half_period);
+
+            leds[x].off().ok();
+            delay.delay_ms(half_period);
+        }
+    }
 }
--- src/08-leds-again/src/main.rs
+++ src/08-leds-again/src/main.rs
@@ -4,11 +4,30 @@
 
 use aux8::entry;
 
+fn power_on_port_e(rcc: &RegisterBlock) -> () {
+    rcc.ahbenr.modify(|_, w| w.iopeen().set_bit());
+}
+
+fn configure_port_e_pins_to_output(gpioe: &RegisterBlock) -> () {
+    gpioe.moder.modify(|_, w| {
+        w.moder8().output();
+        w.moder9().output();
+        w.moder10().output();
+        w.moder11().output();
+        w.moder12().output();
+        w.moder13().output();
+        w.moder14().output();
+        w.moder15().output()
+    });
+}
+
 #[entry]
 fn main() -> ! {
     let (gpioe, rcc) = aux8::init();
 
-    // TODO initialize GPIOE
+    power_on_port_e(&rcc);
+
+    configure_port_e_pins_to_output(&gpioe);
 
     // Turn on all the LEDs in the compass
     gpioe.odr.write(|w| {
